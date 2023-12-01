da3b201cd008eff3d7485a535409dac7f25b9a83
solemode404 <146377506+solemode404@users.noreply.github.com>
2023-12-01 18:55:12 -0500

    background music JS

--- fifteen.js
+++ fifteen.js
@@ -174,3 +174,20 @@
 		notification.classList.remove("open-notification");
         notification1.classList.remove("open-notification1");
 	}
+
+//Background Music
+
+
+var mySong= document.getElementById("mySong");
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    if(mySong.paused){
+        mySong.play();
+        icon.src ="./img/pause.png";
+    }
+    else{
+        mySong.pause();
+        icon.src ="./img/play.png";
+    }
+}
